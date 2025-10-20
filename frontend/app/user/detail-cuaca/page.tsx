ce925558f3ff21db0cfc31bf19554172670650f1
Tiara Agustin <tiaraagustin7072@gmail.com>
2025-10-20 20:32:38 +0700

    2

--- frontend/app/user/detail-cuaca/page.tsx
+++ frontend/app/user/detail-cuaca/page.tsx
@@ -2,11 +2,8 @@ import Header from '@/app/components/Header';
 import Footer from '@/app/components/Footer';
 import WeatherDetail from '@/app/components/detailCuaca/WeatherDetail';
 
-const base_url = "https://jsonplaceholder.typicode.com/posts";
 
-export default async function DetailCuacaPage() {
-  const response = await fetch(base_url);
-  const data = await response.json();
+export default function DetailCuacaPage() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
@@ -15,7 +12,6 @@ export default async function DetailCuacaPage() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-8">
           <h1 className="text-3xl font-bold text-gray-900">Detail Cuaca</h1>
         </div>
-        <p className="text-xl text-gray-900">{data[0].title}</p>
         <WeatherDetail />
       </main>
       <Footer />
